d5bad2745a1293d624c2115503c97c852975fd2f
Sumit <kumar76sumit@gmail.com>
2023-11-14 22:15:45 +0530

    Override

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -6,17 +6,3 @@ public class Main {
         obj.meth();
     }
 }
-class A
-{
-    String meth()
-    {
-        return "Invoking method from class A";
-    }
-}
-class B extends A
-{
-    String meth()
-    {
-        return "Method is overridden in Extendend class B";
-    }
-}
